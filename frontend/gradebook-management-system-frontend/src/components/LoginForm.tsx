25d2258a1c755586ff5e0e827eb359116af18eb0
Henry <henry.a.s.arrovin@gmail.com>
2024-04-20 21:04:36 +0530

    created LoginForm component.

---
+++ frontend/gradebook-management-system-frontend/src/components/LoginForm.tsx
@@ -0,0 +1,69 @@
+import APlusLogo from "../assets/A+.jpg";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const LoginForm = () => {
+  const loginSchema = z.object({
+    username: z.string().min(1, { message: "Username is required" }),
+    password: z.string().min(1, { message: "Password is required" }),
+  });
+
+  type Login = z.infer<typeof loginSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isValid },
+  } = useForm<Login>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = (data: Login) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <img src={APlusLogo} alt="A+ logo" width={100} height={100} />
+      <h1>Login Form</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label">
+            Username
+          </label>
+          <input
+            {...register("username")}
+            id="username"
+            className="form-control"
+            type="text"
+            placeholder="Username"
+          />
+          {errors.username && (
+            <p className="text-danger">{errors.username.message}</p>
+          )}
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            {...register("password")}
+            id="password"
+            className="form-control"
+            type="password"
+            placeholder="Password"
+          />
+          {errors.password && (
+            <p className="text-danger">{errors.password.message}</p>
+          )}
+        </div>
+        <button disabled={!isValid} className="btn btn-primary" type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
