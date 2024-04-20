100bce79eadd4b8cc93a19efabc64733c61f89c8
Henry <henry.a.s.arrovin@gmail.com>
2024-04-20 21:04:41 +0530

    made changes.

--- frontend/gradebook-management-system-frontend/src/App.tsx
+++ frontend/gradebook-management-system-frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import LoginForm from "./components/LoginForm";
 
 function App() {
   return (
     <>
       <h1>Grade Book Mangement System.</h1>
+      <LoginForm />
     </>
   );
 }
