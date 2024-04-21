cd39fd9b0c1d614e224a2027ab28defb9694eb4b
Henry <henry.a.s.arrovin@gmail.com>
2024-04-22 02:52:30 +0530

    enabled cors

--- gradebook-management-system/src/main/java/com/project/gradebookmanagementsystem/controllers/CourseController.java
+++ gradebook-management-system/src/main/java/com/project/gradebookmanagementsystem/controllers/CourseController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/courses")
+@CrossOrigin(origins = "http://localhost:5173")
 public class CourseController {
 
     private CourseService courseService;
